5faa4ac9ff2f753fc6f0557e494ed4cf2ae24dab
MDolega20 <mateusz.dolega.20@gmail.com>
2019-09-16 13:44:11 +0200

    wrk

--- lib/model/MusicModel.dart
+++ lib/model/MusicModel.dart
@@ -1,27 +1,51 @@
-import 'package:flute_music_player/flute_music_player.dart';
+//import 'package:flute_music_player/flute_music_player.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 // https://github.com/iampawan/Flutter-Music-Player/blob/master/lib/data/song_data.dart
+// https://github.com/florent37/Flutter-AssetsAudioPlayer
 
 
 class MusicModel extends Model{
-  Song _song;
-  List<Song> recent, favorite, songs;
+  Track _song;
+  List<Track> recent, favorite, songs;
   bool loading = false;
   String error;
 
-  Song get song => _song;
+
+
+//  Song get song => _song;
 
   findSounds() async {
-    try {
-      loading = true;
-      var _songs = await MusicFinder.allSongs();
-      songs = List.from(_songs);
-    } catch (e) {
-      error = e.messege;
-      print("Failed to get songs: '${e.message}'.");
-    } finally{
-      loading = false;
-    }
+    loading = true;
+    List<Track> _tracks = [
+      Track("Nazwa utworu 1","Autor", "track01.mp3", "music"),
+      Track("Nazwa utworu 2","Autor", "track02.mp3", "music"),
+      Track("Nazwa utworu 3","Autor", "track03.mp3", "music"),
+    ];
+
+    songs = _tracks;
+    loading = false;
+
+//    try {
+//      MusicFinder audioPlayer = new MusicFinder();
+//
+//      loading = true;
+//      var _songs = await MusicFinder.allSongs();
+//      songs = List.from(_songs);
+//    } catch (e) {
+//      error = e.messege;
+//      print("Failed to get songs: '${e.message}'.");
+//    } finally{
+//      loading = false;
+//    }
   }
+}
+
+class Track {
+  String title;
+  String author;
+  String assetName;
+  String assetFolder;
+
+  Track(title, author, assetName, assetFolder);
 }
